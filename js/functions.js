d1316426d07bddf6094693b0640b9eef8c6286ed
YaroslavNE-Frontend <yaroslav.nov2001@gmail.com>
2024-02-18 19:50:40 +0900

    Добавляет задание 1 модуля 2

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+function task_one(line, lgth) {
+  line = "проверяемая строка";
+  lgth = 20;
+  let results_task_one = line.length <= lgth;
+  console.log(results_task_one);
+  alert(results_task_one);
+}
+
+task_one();
+
+function task_two(line, lgth) {
+  line = "проверяемая строка";
+  lgth = 20;
+}
+
+task_two();
+
+
