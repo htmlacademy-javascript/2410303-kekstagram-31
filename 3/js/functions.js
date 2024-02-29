93a069e31fb223d3b9049dbde5e33fab926eeb06
Keks <keks@htmlacademy.ru>
2024-02-29 05:57:39 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,31 @@
+// Проверка длины строки
+
+function StringLength(line, lgth) {
+  return line.length <= lgth;
+}
+
+StringLength('проверяемая строка', 20);
+
+// Проверка является ли слово палиндромом
+
+function Palindrom(string) {
+  const lowerString = string.toLowerCase();
+  const characters = lowerString.replaceAll(/[^а-яa-z1-9]/gi, '');
+  const len = characters.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (characters[i] !== characters[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+Palindrom('');
+
+
+// // Строка является палиндромом
+// имяФункции('топот'); // true
+// // Несмотря на разный регистр, тоже палиндром
+// имяФункции('ДовОд'); // true
+// // Это не палиндром
+// имяФункции('Кекс');  // false
