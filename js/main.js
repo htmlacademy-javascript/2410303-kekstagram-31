3f40f68f0b79ad8d436c5be5f3678e473cd4c072
YaroslavNE-Frontend <yaroslav.nov2001@gmail.com>
2024-03-06 18:04:08 +0900

    Добавляет изображения на страницу 2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 import { createdPosts } from './create-posts.js';
 // console.log(createdPosts());
 
-const body = document.body;
 const similarPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const similarPhotos = createdPosts();
+const pictures = document.querySelector('.pictures');
 
 
 similarPhotos.forEach((post) => {
   const PhotoElement = similarPhotoTemplate.cloneNode(true);
-  const container = document.createElement('div');
   PhotoElement.querySelector('.picture__img').src = post.url;
   PhotoElement.querySelector('.picture__img').alt = post.description;
   PhotoElement.querySelector('.picture__likes').textContent = post.likes;
   PhotoElement.querySelector('.picture__comments').textContent = post.comments.length;
-  body.appendChild(container);
-  container.classList.add('container');
-  container.appendChild(PhotoElement);
+  pictures.appendChild(PhotoElement);
 });
 
 
